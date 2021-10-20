f30460a55200f5ddd0ff04c15f553eabb34d0e6f
Peter Coyne <g00266507@gmit.ie>
2021-10-20 10:49:15 +0100

    first commit

---
+++ components/create.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export class Create extends Component {
+    render() {
+        return(
+            <div><h1>This is the Create component</h1></div>
+        );
+    }
+}
